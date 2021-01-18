d8dc3eb6489292f054cd2b72b24d16dda4675540
anh.tran(c0720i1) <69283798+theanh111@users.noreply.github.com>
2021-01-18 10:33:49 +0700

    Create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MySongComponent } from './client/user/my-song/my-song.component';
+import { MyProfileComponent } from './client/user/my-profile/my-profile.component';
+import { UpdateProfileComponent } from './client/user/update-profile/update-profile.component';
+import { CreateSongComponent } from './song/create-song/create-song.component';
+import { ListSongComponent } from './song/list-song/list-song.component';
+import { UpdateSongComponent } from './song/update-song/update-song.component';
+import { PlaySongComponent } from './song/play-song/play-song.component';
+import { LoginComponent } from './visiter/login/login.component';
+import { RegisterComponent } from './visiter/register/register.component';
+import { ResetpasswordComponent } from './client/user/resetpassword/resetpassword.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MySongComponent,
+    MyProfileComponent,
+    UpdateProfileComponent,
+    CreateSongComponent,
+    ListSongComponent,
+    UpdateSongComponent,
+    PlaySongComponent,
+    LoginComponent,
+    RegisterComponent,
+    ResetpasswordComponent
   ],
   imports: [
     BrowserModule
