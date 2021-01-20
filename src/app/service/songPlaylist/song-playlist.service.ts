9a6fc1aa529d6c71e250f3aae8033a196455cf53
Khanh-Nobi-JR <69251285+Khanh-Nobi-JR@users.noreply.github.com>
2021-01-20 23:32:46 +0700

    not submit playlist

---
+++ src/app/service/songPlaylist/song-playlist.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+import {Observable} from 'rxjs';
+import {ISong} from '../../model/song/ISong';
+
+const UPR_API = `${environment.apiUrl}`
+@Injectable({
+  providedIn: 'root'
+})
+export class SongPlaylistService {
+
+  constructor(private httpClient: HttpClient) { }
+  addSongToPlaylist(p_id: number, song: ISong): Observable<any> {
+    return this.httpClient.post<ISong>(UPR_API + `/song-playlists/${p_id}`, song);
+  }
+}
